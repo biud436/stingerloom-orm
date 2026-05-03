2b82810e0f26308a34b26d2da3f20f388812673e
biud436 <biud436@gmail.com>
2026-05-03 23:12:38 +0900

    refactor: improve code readability by adding braces for conditional statements

--- examples/nestjs-multitenant/src/users/users.service.ts
+++ examples/nestjs-multitenant/src/users/users.service.ts
@@ -21,7 +21,10 @@ export class UsersService implements OnModuleInit {
     const user = new User();
     user.username = dto.username;
     user.email = dto.email;
-    if (dto.bio) user.bio = dto.bio;
+
+    if (dto.bio) {
+      user.bio = dto.bio;
+    }
 
     return await this.userRepository.save(user);
   }
@@ -32,16 +35,28 @@ export class UsersService implements OnModuleInit {
 
   async findOne(id: number): Promise<User> {
     const result = await this.userRepository.findOne({ where: { id } });
-    if (!result) throw new NotFoundException(`User with ID ${id} not found`);
+
+    if (!result) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+
     return result;
   }
 
   async update(id: number, dto: UpdateUserDto): Promise<User> {
     const user = await this.findOne(id);
 
-    if (dto.username !== undefined) user.username = dto.username;
-    if (dto.email !== undefined) user.email = dto.email;
-    if (dto.bio !== undefined) user.bio = dto.bio;
+    if (dto.username !== undefined) {
+      user.username = dto.username;
+    }
+
+    if (dto.email !== undefined) {
+      user.email = dto.email;
+    }
+
+    if (dto.bio !== undefined) {
+      user.bio = dto.bio;
+    }
 
     return await this.userRepository.save(user);
   }
